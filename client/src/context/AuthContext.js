d0463027cc3877d234e1b2b8341c75a4f6c55555
Priya Kalamkar <priyakalamkar555@gmail.com>
2026-04-25 23:32:12 +0530

    Update AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
 // ADD THIS LINE HERE:
-axios.defaults.baseURL = 'https://brahmastra-djng.onrender.com';
+axios.defaults.baseURL = 'https://brahmastra-djng.onrender.com/';
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
